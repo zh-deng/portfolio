6ac330ee1aa28b3bc7b5d8a01dfc0259bdddea94
zh-deng <heromemo@web.de>
2023-03-08 00:04:59 +0100

    bug fix another try netlify

--- src/containers/contactform/Contactform.jsx
+++ src/containers/contactform/Contactform.jsx
@@ -11,7 +11,7 @@ const Contactform = () => {
     return (
         <div className="contact" id="contact">
             <div className="contact__contactform-container">
-                <form name="contactform" method="POST" date-netlify="true" onSubmit="submit">
+                <form name="contactform" method="POST">
                     <input type="hidden" name="form-name" value="contactform" />
                     <h2>
                         Kontaktieren Sie mich
